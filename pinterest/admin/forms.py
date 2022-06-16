1b9525d5ec41d0fecbfbf4a0e5b8d37dd343ceaf
chitrangda <chitrangda.inexture@gmail.com>
2022-06-16 11:54:44 +0530

    added update profile and admin portion

--- pinterest/admin/forms.py
+++ pinterest/admin/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField,  SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 
 from pinterest.models import Category
@@ -8,11 +8,12 @@ from pinterest.models import Category
 
 class NewCategory(FlaskForm):
     """New Category Form"""
-    category_name=StringField('Category Name',validators=[DataRequired(), Length(min=2, max=20)])
+    category_name = StringField('Category Name', validators=[DataRequired(), Length(min=2, max=20)])
     submit = SubmitField('Add')
 
     def validate_category_name(self, category_name):
         """for validation of unique category"""
-        category = Category.query.filter_by(category_name=category_name.data).first()
+        category_lower = (category_name.data.lower())
+        category = Category.query.filter_by(category_name=category_lower).first()
         if category:
             raise ValidationError('Oops,Category already exists!')
